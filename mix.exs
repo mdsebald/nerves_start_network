3f8983a47c873e00cf0f6d3f62d0f2a2776c15cc
Mark Sebald <mdsebald@users.noreply.github.com>
2017-03-16 15:37:55 -0500

    Update to match Nerves version 0.5.0 changes

--- mix.exs
+++ mix.exs
@@ -1,8 +1,14 @@
 defmodule NervesStartNetwork.Mixfile do
   use Mix.Project
 
-  @target System.get_env("NERVES_TARGET") || "rpi3"
+  @target System.get_env("MIX_TARGET") || "host"
 
+  Mix.shell.info([:green, """
+  Env
+    MIX_TARGET:   #{@target}
+    MIX_ENV:      #{Mix.env}
+  """, :reset])
+  
   def project do
     [app: :nerves_start_network,
      version: "0.1.0",
@@ -13,32 +19,61 @@ defmodule NervesStartNetwork.Mixfile do
      target: @target,
      deps_path: "deps/#{@target}",
      build_path: "_build/#{@target}",
-     aliases: aliases(),
-     deps: deps() ++ system(@target)]
+     aliases: aliases(@target),
+     deps: deps()]
   end
 
-  def application do
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application, do: application(@target)
+  
+  # Specify target specific application configurations
+  # It is common that the application start function will start and supervise
+  # applications which could cause the host to fail. Because of this, we only
+  # invoke StartNetwork.start/2 when running on a target.
+  def application("host") do
+    [extra_applications: [:logger,
+                          :nerves_networking,
+                          :nerves_ssdp_server,
+                          :nerves_lib]]
+  end
+  def application(_target) do
     [mod: {StartNetwork, []},
-     applications: [:logger,
-                    :nerves_networking,
-                    :nerves_ssdp_server,
-                    :nerves_lib]]
+     extra_applications: [:logger,
+                          :nerves_networking,
+                          :nerves_ssdp_server,
+                          :nerves_lib]]
   end
 
-  defp deps do
-    [{:nerves, "~> 0.5.0"},
+  # Dependencies can be Hex packages:
+  #
+  #   {:my_dep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
+   def deps do
+    [{:nerves, "~> 0.5.0", runtime: false}] ++
+    deps(@target)
+  end
+  
+  # Specify target specific dependencies
+  def deps("host"), do: []
+  def deps(target) do
+    [{:nerves_runtime, "~> 0.1.0"},
+     {:"nerves_system_#{target}", "~> 0.11.0"},
      {:nerves_lib, github: "nerves-project/nerves_lib"},
      {:nerves_networking, github: "nerves-project/nerves_networking"},
      {:nerves_ssdp_server, github: "nerves-project/nerves_ssdp_server"}]
   end
-
-  def system(target) do
-    [
-     {:"nerves_system_#{target}", "~> 0.11.0"}
-    ]
-  end
-
-  def aliases do
+  
+  # We do not invoke the Nerves Env when running on the Host
+  def aliases("host"), do: []
+  def aliases(_target) do
     ["deps.precompile": ["nerves.precompile", "deps.precompile"],
      "deps.loadpaths":  ["deps.loadpaths", "nerves.loadpaths"]]
   end
